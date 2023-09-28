dde9b8e830af3a9c3c78f770a21c11f08022408b
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-09-28 16:58:17 +0300

    Update mylist.rb

--- mylist.rb
+++ mylist.rb
@@ -1,2 +1,11 @@
-
-
+require_relative 'my_enumerable'
+class MyList
+  include MyEnumerable
+  attr_reader :list
+  def initialize(*items)
+    @list = items
+  end
+  def each(&block)
+    @list.each(&block)
+  end
+end 
