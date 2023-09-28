1f60c1a2b05d87864288088fc7f8cc945603ac81
Ochiengsteven <stevenochieng6305@gmail.com>
2023-09-28 17:08:18 +0300

    ADD ENUMERABLES

--- mylist.rb
+++ mylist.rb
@@ -8,4 +8,16 @@ class MyList
   def each(&block)
     @list.each(&block)
   end
-end 
+end
+
+list = MyList.new(1, 2, 3, 4, 5)
+puts(list.all? { |x| x < 6 })
+# true
+puts(list.all? { |e| e > 5 })
+# false
+puts(list.any? { |e| e == 2 })
+# true
+puts(list.any? { |e| e == 6 })
+# false
+print(list.filter(&:even?))
+# [2, 4]
